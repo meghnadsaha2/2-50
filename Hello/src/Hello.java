5d4c5f754312a50e64c43917010f3ca47c390eda
meghnadsaha <meghnadsaha422@gmail.com>
2017-03-02 17:50:56 +0530

    22

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 
 		 int arr[]={1,2,3,4,5,6,7,8,9,10};  
 		  
-		   for(int i:arr22255){  
+		   for(int i:arr222522222222222222){  
 		     System.out.println(i);  
 		   }  
 		
