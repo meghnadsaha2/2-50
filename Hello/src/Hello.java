68ba8be88f457b412c016734fc0104f5810904f0
meghnadsaha <meghnadsaha422@gmail.com>
2017-03-02 16:26:42 +0530

    From USER 16:26

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,11 +4,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Owner");
+		for(int i = 0 j < 9 ; i++) {
+			System.out.println(i);
+		}
 		
-		
-		System.out.println("User");
-		System.out.println("1111111111111111111 ");
 		
 		
 		
