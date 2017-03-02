50028e9d68abb7030b079de565e1381a9bf8c30a
meghnadsaha <meghnadsaha422@gmail.com>
2017-03-02 16:44:06 +0530

    iiii

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i = 0 j < 9 ; i++) {
+		for(int i = 0 ;i < 9 ; i++) {
 			System.out.println(i);
 		}
 		
